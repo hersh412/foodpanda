4740d402ba20f2892be28f3e59647f3a292e329f
hersh412 <jdh428@gmail.com>
2024-02-28 13:39:56 -0500

    first

---
+++ seller_app/lib/authentication/register.dart
@@ -0,0 +1,217 @@
+import 'dart:io';
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:seller_app/widgets/custom_text_field.dart';
+import 'package:seller_app/widgets/error_dialog.dart';
+
+class RegisterScreen extends StatefulWidget {
+  const RegisterScreen({super.key});
+
+  @override
+  State<RegisterScreen> createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  TextEditingController nameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController confirmPasswordController = TextEditingController();
+  TextEditingController phoneController = TextEditingController();
+  TextEditingController locationController = TextEditingController();
+
+  XFile? imageXFile;
+  final ImagePicker _picker = ImagePicker();
+
+  Position? position;
+  List<Placemark>? placemarks;
+
+  late bool serviceEnabled;
+
+  late LocationPermission permission;
+
+  Future<void> formValidation() async {
+    if (imageXFile == null) {
+      showDialog(context: context , builder: (c) {
+        return const ErrorDialog("Please select an image.");
+      }
+      );
+    }
+
+  }
+
+  getCurrentLocation() async {
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    Position newPosition = await Geolocator.getCurrentPosition(
+    desiredAccuracy: LocationAccuracy.high,
+    );
+
+    position = newPosition;
+    placemarks = await placemarkFromCoordinates(position!.latitude, position!.longitude);
+    Placemark pmark = placemarks!.first;
+    String completeAddress = '${pmark.subThoroughfare} ${pmark.thoroughfare}, ${pmark.subLocality}, ${pmark.locality}, ${pmark.subAdministrativeArea}, ${pmark.administrativeArea} ${pmark.postalCode}, ${pmark.country}';
+    locationController.text = completeAddress;
+  }
+
+  Future<void> _getImage() async {
+    imageXFile = await _picker.pickImage(source: ImageSource.gallery);
+    setState(() {
+      imageXFile;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Container(
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            InkWell(
+              onTap: () {
+                _getImage();
+              },
+              child: CircleAvatar(
+                  radius: MediaQuery.of(context).size.width * 0.20,
+                  backgroundColor: Colors.white,
+                  backgroundImage: imageXFile == null
+                      ? null
+                      : FileImage(File(
+                          imageXFile!.path,
+                        )),
+                  child: imageXFile == null
+                      ? Icon(
+                          Icons.add_photo_alternate,
+                          size: MediaQuery.of(context).size.width * 0.20,
+                          color: Colors.grey,
+                        )
+                      : null),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  CustomTextField(
+                    data: Icons.person,
+                    controller: nameController,
+                    hintText: "Name",
+                    isObscure: false,
+                  ),
+                  CustomTextField(
+                    data: Icons.email,
+                    controller: emailController,
+                    hintText: "Email",
+                    isObscure: false,
+                  ),
+                  CustomTextField(
+                    data: Icons.password,
+                    controller: passwordController,
+                    hintText: "Password",
+                    isObscure: true,
+                  ),
+                  CustomTextField(
+                    data: Icons.password,
+                    controller: confirmPasswordController,
+                    hintText: "Confirm Password",
+                    isObscure: true,
+                  ),
+                  CustomTextField(
+                    data: Icons.phone,
+                    controller: phoneController,
+                    hintText: "Phone",
+                    isObscure: false,
+                  ),
+                  CustomTextField(
+                    data: Icons.my_location,
+                    controller: locationController,
+                    hintText: "Cafe/Restaurant Address",
+                    isObscure: false,
+                    enabled: false,
+                  ),
+                  Container(
+                    width: 400,
+                    height: 40,
+                    alignment: Alignment.center,
+                    child: ElevatedButton.icon(
+                      label: const Text(
+                        "Get Location",
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                      icon: const Icon(
+                        Icons.location_on,
+                        color: Colors.white,
+                      ),
+                      style: ElevatedButton.  styleFrom(
+                          backgroundColor: Colors.amber,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30),
+                          )),
+                      onPressed: () => getCurrentLocation(),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.purple,
+                padding: EdgeInsets.symmetric(horizontal: 40, vertical: 10)
+              ),
+              onPressed: () {
+                 formValidation();
+              },
+              child: const Text(
+                "Sign Up",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            const SizedBox(height: 30,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ seller_app/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:seller_app/splashScreen/splash_screen.dart';
+
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Sellers App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurpleAccent),
+        useMaterial3: true,
+      ),
+      home: const MySplashScreen(),
+    );
+  }
+}
